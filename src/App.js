84701843bdca39f738c6eb383c2099e15aff9d32
Aarhan Khan <aarhankhan212@gmail.com>
2022-09-02 04:42:37 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import MainDashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
+
 function App() {
   return (
     <Router>
